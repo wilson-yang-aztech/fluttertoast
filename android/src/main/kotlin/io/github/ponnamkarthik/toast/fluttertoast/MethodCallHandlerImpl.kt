11d835cc83d4b99b0fa0acfe62429dbe4e956256
wilson <wilson.yang@aztech.com>
2026-03-24 10:29:47 +0800

    first commit

---
+++ android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/MethodCallHandlerImpl.kt
@@ -0,0 +1,165 @@
+package io.github.ponnamkarthik.toast.fluttertoast
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.AssetManager
+import android.graphics.PorterDuff
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.view.FlutterMain
+import java.util.Locale
+
+enum class FontSizeUnit(val code: String) {
+
+    PX("px"), DP("dp"), SP("sp");
+
+    companion object {
+        fun formCode(code: String?): FontSizeUnit {
+            if (code == null) return SP
+            return values().firstOrNull { it.code == code.lowercase() } ?: SP
+        }
+    }
+}
+
+internal class MethodCallHandlerImpl(private var context: Context) : MethodCallHandler {
+
+    private var mToast: Toast? = null
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        Log.i("onMethodCall", "onMethodCall    result:${result}")
+        when (call.method) {
+            "showToast" -> {
+                val mMessage = call.argument<Any>("msg").toString()
+                val length = call.argument<Any>("length").toString()
+                val gravity = call.argument<Any>("gravity").toString()
+                val bgcolor = call.argument<Number>("bgcolor")
+                val textcolor = call.argument<Number>("textcolor")
+                val fontSize = call.argument<Number>("fontSize")
+                val fontSizeUnit = FontSizeUnit.formCode(call.argument<String>("fontSizeUnit"))
+                val fontAsset = call.argument<String>("fontAsset")
+
+                val mGravity: Int = when (gravity) {
+                    "top" -> Gravity.TOP
+                    "center" -> Gravity.CENTER
+                    else -> Gravity.BOTTOM
+                }
+
+                val mDuration: Int = if (length == "long") {
+                    Toast.LENGTH_LONG
+                } else {
+                    Toast.LENGTH_SHORT
+                }
+
+                if (bgcolor != null) {
+                    val layout = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.toast_custom, null)
+                    val text = layout.findViewById<TextView>(R.id.text)
+                    text.text = mMessage
+
+                    val gradientDrawable: Drawable? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        context.getDrawable(R.drawable.corner)!!
+                    } else {
+                       // context.resources.getDrawable(R.drawable.corner)
+                        ContextCompat.getDrawable(context, R.drawable.corner)
+                    }
+                    gradientDrawable!!.setColorFilter(bgcolor.toInt(), PorterDuff.Mode.SRC_IN)
+                    text.background = gradientDrawable
+
+                    if (fontSize != null) {
+                        when (fontSizeUnit) {
+                            FontSizeUnit.SP -> text.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize.toFloat())
+                            FontSizeUnit.PX -> text.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize.toFloat())
+                            FontSizeUnit.DP -> text.setTextSize(TypedValue.COMPLEX_UNIT_DIP, fontSize.toFloat())
+                        }
+                        Log.i("onMethodCall", "fontSizeUnit: $fontSizeUnit  fontSize: $fontSize")
+                    }
+                    if (textcolor != null) {
+                        text.setTextColor(textcolor.toInt())
+                    }
+
+                    mToast = Toast(context)
+                    mToast?.duration = mDuration
+
+                    if (fontAsset != null) {
+                        val assetManager: AssetManager = context.assets
+                        val key = FlutterMain.getLookupKeyForAsset(fontAsset)
+                        text.typeface = Typeface.createFromAsset(assetManager, key);
+                    }
+                    mToast?.view = layout
+                } else {
+
+                    mToast = Toast.makeText(context, mMessage, mDuration)
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+                        val textView: TextView = mToast?.view!!.findViewById(android.R.id.message)
+                        if (fontSize != null) {
+                            when (fontSizeUnit) {
+                                FontSizeUnit.SP -> textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize.toFloat())
+                                FontSizeUnit.PX -> textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize.toFloat())
+                                FontSizeUnit.DP -> textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, fontSize.toFloat())
+                            }
+                            Log.i("onMethodCall", "fontSizeUnit: $fontSizeUnit  fontSize: $fontSize")
+                        }
+                        if (textcolor != null) {
+                            textView.setTextColor(textcolor.toInt())
+                        }
+                        if (fontAsset != null) {
+                            val assetManager: AssetManager = context.assets
+                            val key = FlutterMain.getLookupKeyForAsset(fontAsset)
+                            textView.typeface = Typeface.createFromAsset(assetManager, key);
+                        }
+                    }
+                }
+
+                try {
+                    when (mGravity) {
+                        Gravity.CENTER -> {
+                            mToast?.setGravity(mGravity, 0, 0)
+                        }
+                        Gravity.TOP -> {
+                            mToast?.setGravity(mGravity, 0, 100)
+                        }
+                        else -> {
+                            mToast?.setGravity(mGravity, 0, 100)
+                        }
+                    }
+                } catch (e: Exception) { }
+
+                if (context is Activity) {
+                    (context as Activity).runOnUiThread { mToast?.show() }
+                } else {
+                    mToast?.show()
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    mToast?.addCallback(
+                        object : Toast.Callback() {
+                            override fun onToastHidden() {
+                                super.onToastHidden()
+                                mToast = null
+                            }
+                        },
+                    )
+                }
+                result.success(true)
+            }
+            "cancel" -> {
+                if (mToast != null) {
+                    mToast?.cancel()
+                    mToast = null
+                }
+                result.success(true)
+            }
+            else -> result.notImplemented()
+        }
+    }
+}
